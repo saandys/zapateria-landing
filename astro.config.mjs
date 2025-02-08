ab62714974c60977b4addf2f62036bea73482770
Sandra Montero <sms125@alu.ua.es>
2025-02-08 18:06:57 +0100

    4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  base: "/zapateria-landing/docs/",
   integrations: [react(), tailwind()]
 });
