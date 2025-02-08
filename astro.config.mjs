ee79e34bad65137b878d260d5e70ff0984843964
Sandra Montero <sms125@alu.ua.es>
2025-02-08 20:52:54 +0100

    añadiendo cosas a ver si funciona

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://saandys.github.io', // Tu URL base
+  base: '/zapateria-landing/', // Esto es importante, ya que indica que estás sirviendo desde /zapateria-landing/docs
   integrations: [react(), tailwind()],
-  buildOptions: {
-    site: 'https://saandys.github.io/zapateria-landing',
-  },
+
 });
